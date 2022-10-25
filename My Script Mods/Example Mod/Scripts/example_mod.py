496833e210d6d8dfbf8a2382017adae6123e3848
jazzyb.21 <jazzyb.207@gmail.com>
2022-10-24 20:31:58 -0500

    money mod and cheat helper

--- My Script Mods/Example Mod/Scripts/example_mod.py
+++ My Script Mods/Example Mod/Scripts/example_mod.py
@@ -13,3 +13,13 @@ import sims4.commands
 def myfirstscript(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     output("Hello World!")
+
+
+@sims4.commands.Command('cheats_help', command_type=sims4.commands.CommandType.Live)
+def hello_world(_connection=None):
+    output = sims4.commands.CheatOutput(_connection)
+    output("add_money <amount>: add money to your current sim")
+    output("remove_money <amount>: remove money from your current sim")
+    output("max_skill <skill name (no space)>: set the skill to max level")
+    output("become_friend <firstname> <lastname>: become friend with the target sim")
+    output("become_lover <firstname> <lastname>: become lover with the target sim")
